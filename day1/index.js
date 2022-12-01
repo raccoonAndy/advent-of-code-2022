33e3b50091d3fc44748148941ec6b4d48ba75ec6
raccoonAndy <byhachi11@gmail.com>
2022-12-01 09:39:51 +0300

    day 1

--- day1/index.js
+++ day1/index.js
@@ -8,12 +8,54 @@ const test = path.resolve(__dirname, 'test.txt');
 
 // Part 1
 const runPartOne = () => {
+  let sum = 0;
+  let max = Number.MIN_VALUE;
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(test),
+  });
+  readInterface.on('line', (line) => {
+    if (line !== '') {
+      let current = parseInt(line);
+      sum += current;
+    } else {
+      max = Math.max(sum, max);
+      sum = 0;
+    }
+  });
+  readInterface.on('close', () => {
+    if (sum != 0) {
+      max = Math.max(sum, max);
+    }
+    console.log('Part 1: ', max);
+  });
+};
+
+runPartOne();
+
+// Part 2
+const runPartTwo = () => {
+  let sum = 0;
+  let allSums = [];
   const readInterface = readline.createInterface({
     input: fs.createReadStream(input),
   });
-  readInterface.on('line', (line) => {});
-  readInterface.on('close', () => {});
+  readInterface.on('line', (line) => {
+    if (line !== '') {
+      let current = parseInt(line);
+      sum += current;
+    } else {
+      allSums.push(sum);
+      sum = 0;
+    }
+  });
+  readInterface.on('close', () => {
+    if (sum != 0) {
+      allSums.push(sum);
+    }
+    let sorted = allSums.sort((a, b) => (a < b ? 1 : -1)).slice(0, 3);
+    let result = sorted.reduce((acc, current) => acc + current, 0);
+    console.log('Part 2: ', result);
+  });
 };
 
-// Part 2
-const runPartTwo = () => {};
+runPartTwo();
